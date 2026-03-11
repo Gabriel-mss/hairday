b38005bc864cb5b9f81e59731cc5fba7f69a5872
Gabriel <gabrielnumero47@gmail.com>
2026-03-11 20:15:49 -0300

    Bloqueando horários e atualizando lista de agendamentos.

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -16,5 +16,5 @@ export async function schedulesDays(){
     schedulesShow({ dailySchedules })
 
     // Renderiza as horas disponíveis.
-    hoursLoad({ date })
+    hoursLoad({ date, dailySchedules })
 }
